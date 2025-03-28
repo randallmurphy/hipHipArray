d9267df16abeefd1aa9c21cf4fa7beab3a0e4336
randall murphy <rmurphy@perseverenow.org>
2025-03-28 10:00:30 -0500

    final commit / assignment complete

--- main.js
+++ main.js
@@ -102,16 +102,31 @@ for(let i = 0; i < newStudents.length;i++){
 
 // Challenge 11
 // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
-
-
+const studentCopy = students.slice(3,11);
+for(let i = 0; i < studentCopy.length;i++){
+  console.log(studentCopy[i]);
+}
+for(let i = 0; i < students.length;i++){
+  console.log(students[i]);
+}
 // Challenge 12
 // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
 const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
-
+const dinoCopy = dinosaurs.splice(4,3);
+for(let i = 0; i < dinoCopy.length;i++){
+  console.log(dinoCopy[i]);
+}
 
 // Challenge 13
 // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
+const dinosaurStrings = dinosaurs.join('*');
+  console.log(dinosaurStrings);
+
 
 
 // Challenge 14
 // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
+dinosaurs.reverse();
+for(let i = 0; i < dinosaurs.length;i++){
+  console.log(dinosaurs[i]);
+}
